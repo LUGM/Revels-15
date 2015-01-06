d4785130553c3aaff38921a136f00a837f00d8e9
shubhsin <luk4shubhs@gmail.com>
2015-01-06 20:55:12 +0530

    Issue in model

---
+++ Revels'15/Events.h
@@ -0,0 +1,13 @@
+//
+//  Events.h
+//  Revels'15
+//
+//  Created by Shubham Sorte on 06/01/15.
+//  Copyright (c) 2015 LUGManipal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Events : NSObject
+
+@end
---
+++ Revels'15/Events.m
@@ -0,0 +1,13 @@
+//
+//  Events.m
+//  Revels'15
+//
+//  Created by Shubham Sorte on 06/01/15.
+//  Copyright (c) 2015 LUGManipal. All rights reserved.
+//
+
+#import "Events.h"
+
+@implementation Events
+
+@end
